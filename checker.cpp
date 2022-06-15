8070da8e55067e4758d58c9c56c92676ab37bd12
dbsic211 <94776938+dbsic211@users.noreply.github.com>
2022-06-15 13:27:49 +0800

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -7,9 +7,9 @@ int main(int argc, char* argv[]) {
     
     int qcnt = ouf.readInt();
     if(qcnt <= 6) quitf(_ok, "Full score");
-    else if(qcnt > 1006) quitf(_wa, "too many queries");
+    else if(qcnt > 70000) quitf(_wa, "too many queries");
     else {
-        double ok = 100 * (1.0 - sqrt(1.0 - (1006 - qcnt) * 1.0 / 1000));
+        double ok = 100 * (1.0 - sqrt(1.0 - pow((70000 - qcnt) * 1.0 / 69994, 20.0)));
         quitp(_pc(ok - 16), "number of queries = %d", qcnt);
     }
 }
